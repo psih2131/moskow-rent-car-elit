f77a853db19bbc534cdf6b04e3d505da3d19be30
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-11-14 14:22:36 +0300

    add certeficats page for pc

--- app/pages/certificates.vue
+++ app/pages/certificates.vue
@@ -1,3 +1,241 @@
 <template>
-    certificates
+    <section class="certificates-hero-sec">
+        <div class="container">
+            <div class="hero-partners-sec__row">
+                
+                <div class="seo-sec-type-1__images">
+                    <img src="@/assets/images/img/sert1.jpg" alt="" class="seo-sec-type-1__img">
+                    <img src="@/assets/images/img/sert2.jpg" alt="" class="seo-sec-type-1__img">
+                </div>
+
+                <div class="seo-sec-type-1__data">
+                    <h1 class="hero-partners-sec__title sec-title sec-title--left-mod sec-title--capitalize-mod">Подарочный сертификат <b>RED ANELS</b> — аренда ЛЮКСОВЫХ авто в Москве</h1>
+
+                    <div class="seo-sec-type-1__text-container">
+                    <div
+                        class="seo-sec-type-1__text-wrapper">
+                        <div class="wp-editor seo-sec-type-1__editor" ref="editor">
+                            <p>Сертификаты на любой номинал. Срок действия — 1 год. Выдаём в офисе или доставим в любую точку Москвы популярными службами доставки, либо в любую точку любого города России службой доставки.</p>
+                        </div>
+                    </div>
+
+                    </div>
+                    
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <section class="certificates-catalog-sec">
+        <div class="container">
+            <div class="header-row-sec">
+                <h2 class="certificates-catalog-sec__title sec-title sec-title--left-mod"><b>ДОСТУПНЫЕ </b> СЕРТИФИКАТЫ</h2>
+            </div>
+
+            <div class="certificates-catalog-sec__wrapper">
+                <div class="certificates-catalog-sec__element certeficat-card">
+                    <div class="certeficat-card__wrapper">
+                        <img src="@/assets/images/img/certeficat-logo.png" alt="" class="certeficat-card__logo">
+                        <p class="certeficat-card__value">25 000₽</p>
+                        <p class="certeficat-card__title">ПОДАРОЧНЫЙ СЕРТИФИКАТ</p>
+                    </div>
+
+                    <div class="certeficat-card__btn-wrapper">
+                        <button class="certeficat-card__btn btnV1 btnV1--big">
+                            <span class="btnV1__circle btnV1__circle-1"></span>
+                            <span class="btnV1__circle btnV1__circle-2"></span>
+                            <span class="btnV1__title">ЗАКАЗАТЬ СЕРТИФИКАТ</span>
+
+                            <div class="btnV1__line btnV1__line-1"></div>
+                            <div class="btnV1__line btnV1__line-2"></div>
+                            <div class="btnV1__line btnV1__line-3"></div>
+                            <div class="btnV1__line btnV1__line-4"></div>
+                        </button>
+                    </div>
+                </div>
+
+
+                <div class="certificates-catalog-sec__element certeficat-card">
+                    <div class="certeficat-card__wrapper">
+                        <img src="@/assets/images/img/certeficat-logo.png" alt="" class="certeficat-card__logo">
+                        <p class="certeficat-card__value">50 000₽</p>
+                        <p class="certeficat-card__title">ПОДАРОЧНЫЙ СЕРТИФИКАТ</p>
+                    </div>
+
+                    <div class="certeficat-card__btn-wrapper">
+                        <button class="certeficat-card__btn btnV1 btnV1--big">
+                            <span class="btnV1__circle btnV1__circle-1"></span>
+                            <span class="btnV1__circle btnV1__circle-2"></span>
+                            <span class="btnV1__title">ЗАКАЗАТЬ СЕРТИФИКАТ</span>
+
+                            <div class="btnV1__line btnV1__line-1"></div>
+                            <div class="btnV1__line btnV1__line-2"></div>
+                            <div class="btnV1__line btnV1__line-3"></div>
+                            <div class="btnV1__line btnV1__line-4"></div>
+                        </button>
+                    </div>
+                </div>
+
+
+                <div class="certificates-catalog-sec__element certeficat-card">
+                    <div class="certeficat-card__wrapper">
+                        <img src="@/assets/images/img/certeficat-logo.png" alt="" class="certeficat-card__logo">
+                        <p class="certeficat-card__value">100 000₽</p>
+                        <p class="certeficat-card__title">ПОДАРОЧНЫЙ СЕРТИФИКАТ</p>
+                    </div>
+
+                    <div class="certeficat-card__btn-wrapper">
+                        <button class="certeficat-card__btn btnV1 btnV1--big">
+                            <span class="btnV1__circle btnV1__circle-1"></span>
+                            <span class="btnV1__circle btnV1__circle-2"></span>
+                            <span class="btnV1__title">ЗАКАЗАТЬ СЕРТИФИКАТ</span>
+
+                            <div class="btnV1__line btnV1__line-1"></div>
+                            <div class="btnV1__line btnV1__line-2"></div>
+                            <div class="btnV1__line btnV1__line-3"></div>
+                            <div class="btnV1__line btnV1__line-4"></div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="certificates-about-sec">
+        <div class="container">
+            <div class="header-row-sec">
+                <h2 class="certificates-about-sec__title sec-title sec-title--left-mod"><b>Сертификат </b> на прокат автомобилей</h2>
+            </div>
+
+            <div class="certificates-about-sec__row">
+                <div class="certificates-about-sec__col">
+                    <div class="certificates-about-sec__text wp-editor">
+                        <p>Сертификаты предоставляют возможность выбора автомобиля любого класса и модели, а также продолжительности аренды. Вы можете выбрать сумму сертификата в соответствии с вашим бюджетом и пожеланиями получателя. Этот подарок подойдет как для любителей автомобилей, так и для тех, кто просто хочет ощутить себя за рулем VIP-авто.</p>
+                    </div>
+                </div>
+
+                <div class="certificates-about-sec__col">
+                    <div class="certificates-about-sec__text wp-editor">
+                        <p>Хотите порадовать близкого человека или коллегу оригинальным и запоминающимся подарком? Подарочные сертификаты на аренду премиум автомобилей - это отличное решение! Наши сертификаты позволят получателю насладиться комфортом и роскошью вождения премиальных автомобилей мировых брендов.</p>
+                    </div>
+                </div>
+
+                <div class="certificates-about-sec__col">
+                    <div class="certificates-about-sec__text wp-editor">
+                        <p>Подарочные сертификаты на аренду премиум автомобилей - это стильный и оригинальный подарок, который оставит яркие впечатления и подарит незабываемые эмоции. Удивите своих близких, друзей или коллег уникальным подарком, который они запомнят на долгие годы!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="partners-how-it-work-sec">
+        <div class="container">
+            <div class="header-row-sec">
+                <h2 class="partners-how-it-work-sec__title sec-title sec-title--left-mod"><b>КАК </b> ПОЛУЧИТЬ СЕРТИФИКАТ</h2>
+
+            </div>
+
+            <div class="partners-how-it-work-sec__row">
+                <div class="partners-how-it-work-sec__element">
+                    <div class="partners-how-it-work-sec__element-counter">
+                        01
+                    </div>
+                    <p class="partners-how-it-work-sec__element-title">ОСТАВИТЬ ЗАЯВКУ</p>
+                    <p class="partners-how-it-work-sec__element-description">Оставьте заявку или напишите в WhatsApp / Telegram</p>
+                </div>
+
+                <div class="partners-how-it-work-sec__element">
+                    <div class="partners-how-it-work-sec__element-counter">
+                        02
+                    </div>
+                    <p class="partners-how-it-work-sec__element-title">ВЫБОР АВТО</p>
+                    <p class="partners-how-it-work-sec__element-description">Укажите понравившийся автомобиль, количество дней аренды и мы забронируем его для вас</p>
+                </div>
+
+                <div class="partners-how-it-work-sec__element">
+                    <div class="partners-how-it-work-sec__element-counter">
+                        03
+                    </div>
+                    <p class="partners-how-it-work-sec__element-title">ПОЛУЧЕНИЕ СЕРТИФИКАТА</p>
+                    <p class="partners-how-it-work-sec__element-description">Приезжайте к нам в офис или закажите доставку сертификата, и после оплаты получите сертификат на руки</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="partners-faq-sec car-faq-sec">
+        <div class="container">
+          <div class="header-row-sec">
+            <h2 class="header-row-sec__title sec-title sec-title--left-mod"><b>УСЛОВИЯ </b> ИСПОЛЬЗЫВАНИЯ</h2>
+
+            <button class="home-hero-sec__btn btnV1 btnV1--big">
+                <span class="btnV1__circle btnV1__circle-1"></span>
+                <span class="btnV1__circle btnV1__circle-2"></span>
+                <span class="btnV1__title">ЗАДАТЬ СВОЙ ВОПРОС</span>
+
+                <div class="btnV1__line btnV1__line-1"></div>
+                <div class="btnV1__line btnV1__line-2"></div>
+                <div class="btnV1__line btnV1__line-3"></div>
+                <div class="btnV1__line btnV1__line-4"></div>
+            </button>
+          </div>
+
+          <div class="faq-wrapper">
+
+            <faqElement :title="'Нужен ли для аренды страховой депозит?'" :counter="'01'"/>
+
+            <faqElement :title="'Где я могу эксплуатировать автомобиль?'" :counter="'02'"/>
+            
+            <faqElement :title="'Возможно ли оформить дополнительного водителя?'" :counter="'03'"/>
+            
+            <faqElement :title="'Как можно получить скидку на аренду автомобиля?'" :counter="'04'"/>
+
+          </div>
+        </div>
+    </section>
+
+    <formSec />
+
+
 </template>
+
+
+
+<script setup>
+
+//IMPORT
+
+import { useCounterStore } from '@/stores/counter'
+
+import { ref, onMounted, onBeforeUnmount, computed, watch  } from 'vue';
+
+// import productCard from '@/components/component__producr-card.vue'
+
+import carCard from '@/components/carCard.vue'
+
+import faqElement from '@/components/faqElement.vue'
+
+import formSec from '@/components/sections/formSec.vue'
+
+
+
+
+//DATA
+// const store = useCounterStore()
+
+// const route = useRoute()
+
+
+
+
+onMounted(() => {
+
+})
+
+</script>
