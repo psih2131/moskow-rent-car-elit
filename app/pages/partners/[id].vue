5f6dccda640f96897e56c3706669d75e4e5b8b80
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-12-01 19:31:43 +0300

    add rest data for single partner page

--- app/pages/partners/[id].vue
+++ app/pages/partners/[id].vue
@@ -1,154 +1,107 @@
 <template>
-    <section class="singl-partner-about-sec">
-        <div class="container">
-            <div class="header-row-sec">
-                <h2 class="singl-partner-about-sec__title sec-title sec-title--left-mod"><b>ИНВИСТИЦИИ </b> В АВТОПАРК</h2>
+    <main class="main" v-if="currentPartnershipData?.length > 0">
 
-                <button class="home-hero-sec__btn btnV1 btnV1--big">
-                    <span class="btnV1__circle btnV1__circle-1"></span>
-                    <span class="btnV1__circle btnV1__circle-2"></span>
-                    <span class="btnV1__title">ОСТАВИТЬ ЗАЯВКУ</span>
+        <section class="singl-partner-about-sec" v-if="currentPartnershipData[0].acf.sekcziya_1_opisanie?.zagolovok">
+            <div class="container">
+                <div class="header-row-sec">
+                    <h2 class="singl-partner-about-sec__title sec-title sec-title--left-mod" v-html="currentPartnershipData[0].acf.sekcziya_1_opisanie.zagolovok"></h2>
 
-                    <div class="btnV1__line btnV1__line-1"></div>
-                    <div class="btnV1__line btnV1__line-2"></div>
-                    <div class="btnV1__line btnV1__line-3"></div>
-                    <div class="btnV1__line btnV1__line-4"></div>
-                </button>
-            </div>
+                    <button class="home-hero-sec__btn btnV1 btnV1--big" @click="openTargetPopupForm()">
+                        <span class="btnV1__circle btnV1__circle-1"></span>
+                        <span class="btnV1__circle btnV1__circle-2"></span>
+                        <span class="btnV1__title">ОСТАВИТЬ ЗАЯВКУ</span>
 
-            <div class="singl-partner-about-sec__wrapper">
-
-                <div class="info-data-row">
-                    <div class="info-data-row__data">
-                    <div class="wp-editor">
-                        <p><b>Мерседес</b> всегда был <b>эталоном</b> автомобилей класса премиум, а Мерседес Гелендваген особенно. Сила, мощь, агрессивный дизайн, черный цвет, грубые, но притягивающие черты, мощность в 571 л.с. и разгон до 100 км за 5,4 секунды – все это Гелендваген (или Гелик, как чаще всего его называют), чем все сказано. Любой праздник превратится в яркое событие, если вы возьмете в аренду Мерседес в Москве. Забудьте о серых и невзрачных праздниках без задора. Аренда Mercedes G63, автомобиля-внедорожника – это ваш шанс выйти за пределы шаблона и по-настоящему почувствовать себя живым ш шанс выйти за пределы шаблона и по-настоящему почувствовать себя живым</p>
-                    </div>
-                    </div>
-                    <div class="info-data-row__img-wrapper">
-                    <img src="@/assets/images/img/pr3.jpg" alt="" class="info-data-row__img">
-                    <img src="@/assets/images/img/pr4.jpg" alt="" class="info-data-row__img">
-                    </div>
+                        <div class="btnV1__line btnV1__line-1"></div>
+                        <div class="btnV1__line btnV1__line-2"></div>
+                        <div class="btnV1__line btnV1__line-3"></div>
+                        <div class="btnV1__line btnV1__line-4"></div>
+                    </button>
                 </div>
 
-                <div class="info-data-row">
-                    <div class="info-data-row__data">
-                    <div class="wp-editor">
-                        <p><b>Мерседес</b> всегда был <b>эталоном</b> автомобилей класса премиум, а Мерседес Гелендваген особенно. Сила, мощь, агрессивный дизайн, черный цвет, грубые, но притягивающие черты, мощность в 571 л.с. и разгон до 100 км за 5,4 секунды – все это Гелендваген (или Гелик, как чаще всего его называют), чем все сказано. Любой праздник превратится в яркое событие, если вы возьмете в аренду Мерседес в Москве. Забудьте о серых и невзрачных праздниках без задора. Аренда Mercedes G63, автомобиля-внедорожника – это ваш шанс выйти за пределы шаблона и по-настоящему почувствовать себя живым ш шанс выйти за пределы шаблона и по-настоящему почувствовать себя живым</p>
+                <div class="singl-partner-about-sec__wrapper">
+
+                    <div class="info-data-row" v-for="value in currentPartnershipData[0].acf.sekcziya_1_opisanie.tekstovye_bloki">
+                        <div class="info-data-row__data">
+                        <div class="wp-editor" v-html="value.tekst"></div>
+                        </div>
+                        <div class="info-data-row__img-wrapper">
+                        <img 
+                        v-for="item in value.kartinki"
+                        :src="item.kartinka.url" :alt="item.kartinka.alt" class="info-data-row__img">
+                        </div>
                     </div>
-                    </div>
-                    <div class="info-data-row__img-wrapper">
-                    <img src="@/assets/images/img/pr7.jpg" alt="" class="info-data-row__img">
-                    <img src="@/assets/images/img/pr8.jpg" alt="" class="info-data-row__img">
-                    </div>
-                </div>
 
-                <div class="info-data-row">
-                    <div class="info-data-row__data">
-                    <div class="wp-editor">
-                        <p><b>Мерседес</b> всегда был <b>эталоном</b> автомобилей класса премиум, а Мерседес Гелендваген особенно. Сила, мощь, агрессивный дизайн, черный цвет, грубые, но притягивающие черты, мощность в 571 л.с. и разгон до 100 км за 5,4 секунды – все это Гелендваген (или Гелик, как чаще всего его называют), чем все сказано. Любой праздник превратится в яркое событие, если вы возьмете в аренду Мерседес в Москве. Забудьте о серых и невзрачных праздниках без задора. Аренда Mercedes G63, автомобиля-внедорожника – это ваш шанс выйти за пределы шаблона и по-настоящему почувствовать себя живым ш шанс выйти за пределы шаблона и по-настоящему почувствовать себя живым</p>
-                    </div>
-                    </div>
-                    <div class="info-data-row__img-wrapper">
-                    <img src="@/assets/images/img/pr9.jpg" alt="" class="info-data-row__img">
-                    <img src="@/assets/images/img/pr10.jpg" alt="" class="info-data-row__img">
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
-
-
-
-    <section class="singl-partner-how-it-work-sec">
-        <div class="container">
-            <div class="header-row-sec">
-                <h2 class="partners-how-it-work-sec__title sec-title sec-title--left-mod"><b>КАК </b> ЭТО РАБОТАЕТ</h2>
-
-                <button class="home-hero-sec__btn btnV1 btnV1--big">
-                    <span class="btnV1__circle btnV1__circle-1"></span>
-                    <span class="btnV1__circle btnV1__circle-2"></span>
-                    <span class="btnV1__title">ОСТАВИТЬ ЗАЯВКУ</span>
-
-                    <div class="btnV1__line btnV1__line-1"></div>
-                    <div class="btnV1__line btnV1__line-2"></div>
-                    <div class="btnV1__line btnV1__line-3"></div>
-                    <div class="btnV1__line btnV1__line-4"></div>
-                </button>
-            </div>
-
-            <div class="partners-how-it-work-sec__row">
-                <div class="partners-how-it-work-sec__element">
-                    <div class="partners-how-it-work-sec__element-counter">
-                        01
-                    </div>
-                    <p class="partners-how-it-work-sec__element-title">Заполняете условия</p>
-                    <p class="partners-how-it-work-sec__element-description">Вы указываете данные а/м и условия аренды: фотографии, минимальный стаж, стоимость и размер депозита</p>
-                </div>
-
-                <div class="partners-how-it-work-sec__element">
-                    <div class="partners-how-it-work-sec__element-counter">
-                        02
-                    </div>
-                    <p class="partners-how-it-work-sec__element-title">Получаете заявки</p>
-                    <p class="partners-how-it-work-sec__element-description">Мы размещаем Ваш авто на сайте иинстаграм, где принимаем заявки на него</p>
-                </div>
-
-                <div class="partners-how-it-work-sec__element">
-                    <div class="partners-how-it-work-sec__element-counter">
-                        03
-                    </div>
-                    <p class="partners-how-it-work-sec__element-title">Зарабатываете деньги</p>
-                    <p class="partners-how-it-work-sec__element-description">Сдаете автомобиль в аренду и получаете деньги с началом аренды</p>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
+        <section class="singl-partner-how-it-work-sec" v-if="currentPartnershipData[0].acf['sekcziya_2_kak_eto_rabotaet']?.zagolovok">
+            <div class="container">
+                <div class="header-row-sec">
+                    <h2 class="partners-how-it-work-sec__title sec-title sec-title--left-mod" v-html="currentPartnershipData[0].acf['sekcziya_2_kak_eto_rabotaet'].zagolovok"></h2>
 
-    <section class="singl-partner-how-start-sec">
-        <div class="container">
-            <div class="header-row-sec">
-                <h2 class="partners-how-start-sec__title sec-title sec-title--left-mod"><b>КАК </b> И С ЧЕГО НАЧАТЬ?</h2>
-            </div>
+                    <button class="home-hero-sec__btn btnV1 btnV1--big" @click="openTargetPopupForm()">
+                        <span class="btnV1__circle btnV1__circle-1"></span>
+                        <span class="btnV1__circle btnV1__circle-2"></span>
+                        <span class="btnV1__title">ОСТАВИТЬ ЗАЯВКУ</span>
 
-            <div class="partners-how-start-sec__row">
-                <div class="partners-how-start-sec__element">
-                    <div class="partners-how-start-sec__element-header">
-                        <div class="partners-how-start-sec__element-header-counter">01</div>
-                        <p class="partners-how-start-sec__element-header-title">Заявка</p>
-                    </div>
-                    <div class="partners-how-start-sec__element-body">
-                        <p class="partners-how-start-sec__element-body-description">Позвоните по телефону: <a href="tel:">+7 925 832 61 07</a> Менеджер поможет оформить страховку, объяснит условия, отправит данные по авто на оценку.</p>
-                    </div>
+                        <div class="btnV1__line btnV1__line-1"></div>
+                        <div class="btnV1__line btnV1__line-2"></div>
+                        <div class="btnV1__line btnV1__line-3"></div>
+                        <div class="btnV1__line btnV1__line-4"></div>
+                    </button>
                 </div>
 
+                <div class="partners-how-it-work-sec__row">
 
-                <div class="partners-how-start-sec__element">
-                    <div class="partners-how-start-sec__element-header">
-                        <div class="partners-how-start-sec__element-header-counter">02</div>
-                        <p class="partners-how-start-sec__element-header-title">ОЦЕНКА</p>
+                    <div class="partners-how-it-work-sec__element" v-for="(value, index) in currentPartnershipData[0].acf['sekcziya_2_kak_eto_rabotaet'].shagi">
+                        <div class="partners-how-it-work-sec__element-counter">
+                            0{{ index + 1 }}
+                        </div>
+                        <p class="partners-how-it-work-sec__element-title" v-html="value.zagolovok"></p>
+                        <p class="partners-how-it-work-sec__element-description" v-html="value.tekst"></p>
                     </div>
-                    <div class="partners-how-start-sec__element-body">
-                        <p class="partners-how-start-sec__element-body-description">В течение дня получите оценочную стоимость вашего авто – она производится по марке, модели, году и характеристикам машины.</p>
-                    </div>
-                </div>
 
-
-                <div class="partners-how-start-sec__element">
-                    <div class="partners-how-start-sec__element-header">
-                        <div class="partners-how-start-sec__element-header-counter">03</div>
-                        <p class="partners-how-start-sec__element-header-title">АРЕНДА</p>
-                    </div>
-                    <div class="partners-how-start-sec__element-body">
-                        <p class="partners-how-start-sec__element-body-description">После подписание клиентского договора, Вы передаете автомобиль нам. С нашей стороны производится установка систем безопасности</p>
-                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <formSec/>
+        <section class="singl-partner-how-start-sec" v-if="currentPartnershipData[0].acf.sekcziya_3_kak_nachat?.zagolovok">
+            <div class="container">
+                <div class="header-row-sec">
+                    <h2 class="partners-how-start-sec__title sec-title sec-title--left-mod" v-html="currentPartnershipData[0].acf.sekcziya_3_kak_nachat.zagolovok"></h2>
+
+                    <button class="home-hero-sec__btn btnV1 btnV1--big" @click="openTargetPopupForm()">
+                        <span class="btnV1__circle btnV1__circle-1"></span>
+                        <span class="btnV1__circle btnV1__circle-2"></span>
+                        <span class="btnV1__title">ОСТАВИТЬ ЗАЯВКУ</span>
+
+                        <div class="btnV1__line btnV1__line-1"></div>
+                        <div class="btnV1__line btnV1__line-2"></div>
+                        <div class="btnV1__line btnV1__line-3"></div>
+                        <div class="btnV1__line btnV1__line-4"></div>
+                    </button>
+
+                </div>
+
+                <div class="partners-how-start-sec__row">
+                    <div class="partners-how-start-sec__element" v-for="(value,index) in currentPartnershipData[0].acf.sekcziya_3_kak_nachat.shagi">
+                        <div class="partners-how-start-sec__element-header">
+                            <div class="partners-how-start-sec__element-header-counter">0{{ index + 1 }}</div>
+                            <p class="partners-how-start-sec__element-header-title" v-html="value.zagolovok"></p>
+                        </div>
+                        <div class="partners-how-start-sec__element-body">
+                            <p class="partners-how-start-sec__element-body-description" v-html="value.tekst"></p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <formSec :formSecData="optionsData" />
+
+    </main>
 
 
 </template>
@@ -158,31 +111,86 @@
 
 //IMPORT
 
-import { useCounterStore } from '@/stores/counter'
-
 import { ref, onMounted, onBeforeUnmount, computed, watch  } from 'vue';
-
-// import productCard from '@/components/component__producr-card.vue'
-
-import carCard from '@/components/carCard.vue'
-
-import faqElement from '@/components/faqElement.vue'
 
 import formSec from '@/components/sections/formSec.vue'
 
+import { useCounterStore } from '@/stores/counter';
+
+import { useNuxtApp } from '#app';
 
 
 
 //DATA
-// const store = useCounterStore()
 
-// const route = useRoute()
+const nuxtApp = useNuxtApp();
+
+const store = useCounterStore(nuxtApp.$pinia);
+
+const route = useRoute()
+
+const { data: currentPartnershipData } = await useFetch(`${store.serverUrlDomainRequest}/wp-json/wp/v2/partnership?slug=${route.params.id}`)
+
+const { data: optionsData } = await useFetch(`${store.serverUrlDomainRequest}/wp-json/acf/v3/options`)
+
+console.log('currentPartnershipData', currentPartnershipData)
+
+console.log('optionsData', optionsData)
 
 
+
+//METHODS
+
+//Open form popup
+const openTargetPopupForm = ()=>{
+  store.changePopupCurrent('popup-form')
+}
 
 
 onMounted(() => {
 
 })
+
+
+
+//SEO
+useHead({
+    title: currentPartnershipData.value[0].acf.seo_title || currentPartnershipData.value[0].title.rendered,
+    meta: [
+        // Description
+        { name: 'description', content: currentPartnershipData.value[0].acf.seo_description || 'Описание по умолчанию' },
+
+        // Keywords (опционально, не влияет сильно на SEO)
+        { name: 'keywords',  content: currentPartnershipData.value[0].acf.klyuchevaya_fraza || 'Авто' },
+
+        // OpenGraph
+        { property: 'og:title', content: currentPartnershipData.value[0].acf.seo_title },
+        { property: 'og:description', content: currentPartnershipData.value[0].acf.seo_description },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: `${store.domainUrlCurrent}${route.fullPath}` },
+        { property: 'og:image', content: currentPartnershipData.value?.[0]?.acf?.og_image?.url || 'http://syberia.gearsdpz.beget.tech/wp-content/uploads/2025/07/87baa9efe5d849e4f8da67fe01f9e029.jpg' },
+
+        // Twitter Card (если используешь)
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: currentPartnershipData.value[0].acf.seo_title },
+        { name: 'twitter:description', content: currentPartnershipData.value[0].acf.seo_description },
+        { name: 'twitter:image', content: currentPartnershipData.value?.[0]?.acf?.og_image?.url || 'http://syberia.gearsdpz.beget.tech/wp-content/uploads/2025/07/87baa9efe5d849e4f8da67fe01f9e029.jpg' },
+
+        // Индексация / Деиндексация
+        // Например, noindex для черновика:
+        {
+        name: 'robots',
+        content:
+            currentPartnershipData.value[0].acf.indeksacziya_v_poiskovyh_sistemah === 'index'
+            ? 'index, follow'
+            : 'noindex, nofollow'
+        }
+    ],
+    link: [
+        // Canonical (вручную или динамически)
+        { rel: 'canonical', href: `${store.domainUrlCurrent}/partners/${currentPartnershipData.value[0].acf.canonical || route.params.id}` }
+    ]
+})
+
 
 </script>
